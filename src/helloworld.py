f3f9ad793b36b281dfc3551db4efc07a17b8e7db
Robert Merkel <robert.merkel@monash.edu>
2017-07-20 17:34:19 +1000

    Non-compiling changes to Hello World

--- src/helloworld.py
+++ src/helloworld.py
@@ -1,3 +1,8 @@
 '''A trivial python program to demonstrate usage of Git'''
 
+#I don't know Python very well so this might break things
+
+for(i = 0; i < 10; i++) {
+   print(i)
+}
 print("hello world")
